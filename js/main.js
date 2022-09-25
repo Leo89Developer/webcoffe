7c345b46b65a2b2e1f06901af55654b42e11b62d
Leonel Ramirez <leo89developer@gmail.com>
2022-09-25 13:03:34 -0600

    Se agrega .gitinore, JS, picutres

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ========*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+
+    if(this.scrollY >=50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== MIXITUP FILTER PRODUCTS ========*/
+let mixerProducts = mixitup('.products__contect', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Default filter products */
+mixerProducts.filter('.delicacies')
+
+/*Link active products*/
+const linkProducts = document.querySelectorAll('.products__item')
+
+function activeProducts(){
+    linkProducts.forEach(l=> l.classList.remove('active-product'))
+    this.classList.add('active-product')
+}
+
+linkProducts.forEach(l=> l.addEventListener('click', activeProducts))
+
